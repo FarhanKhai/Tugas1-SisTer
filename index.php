9fca188914a379dd9f171c3667406a7d1f692033
FarhanKhai <muhammadfarhankhai@gmail.com>
2025-09-21 09:44:11 +0700

    Add main page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- Halaman awal -->
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Perkuliahan</title>
+</head>
+<body>
+    <h1>Dashboard Perkuliahan</h1>
+    <ul>
+        <li><a href="Mahasiswa/index.php">Mahasiswa</a></li>
+        <li><a href="Dosen/index.php">Dosen</a></li>
+        <li><a href="MataKuliah/index.php">Mata Kuliah</a></li>
+        <li><a href="Kuliah/index.php">Kuliah</a></li>
+    </ul>
+</body>
+</html>
